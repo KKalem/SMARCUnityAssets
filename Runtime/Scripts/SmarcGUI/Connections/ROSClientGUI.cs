bb6689aaad8b316c89f34f56cdd436bdd0e6b2a9
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-03-14 17:26:44 +0100

    Added ros publication from unity gamepad input

--- Runtime/Scripts/SmarcGUI/Connections/ROSClientGUI.cs
+++ Runtime/Scripts/SmarcGUI/Connections/ROSClientGUI.cs
@@ -1,9 +1,10 @@
-using System;
-using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Robotics.ROSTCPConnector;
+using UnityEngine.InputSystem;
+
+using RosMessageTypes.Sensor;
 
 namespace SmarcGUI.Connections
 {
@@ -14,11 +15,29 @@ namespace SmarcGUI.Connections
         public TMP_InputField PortInput;
         public Button ConnectButton;
         public Button DisconnectButton;
+        public Toggle PublishControllerToRosToggle;
+        public string JoyTopic = "/joy";
+        InputAction lstick, rstick, lb, rb, lt, rt, north, south, east, west, dpad;
 
         ROSConnection rosCon;
 
         string ServerAddress => ServerAddressInput.text;
         int ServerPort => int.Parse(PortInput.text);
+
+        void Awake()
+        {
+            lstick = InputSystem.actions.FindAction("VirtualJoy/LeftStick");
+            rstick = InputSystem.actions.FindAction("VirtualJoy/RightStick");
+            lb = InputSystem.actions.FindAction("VirtualJoy/LB");
+            rb = InputSystem.actions.FindAction("VirtualJoy/RB");
+            lt = InputSystem.actions.FindAction("VirtualJoy/LT");
+            rt = InputSystem.actions.FindAction("VirtualJoy/RT");
+            north = InputSystem.actions.FindAction("VirtualJoy/North");
+            south = InputSystem.actions.FindAction("VirtualJoy/South");
+            east = InputSystem.actions.FindAction("VirtualJoy/East");
+            west = InputSystem.actions.FindAction("VirtualJoy/West");
+            dpad = InputSystem.actions.FindAction("VirtualJoy/Dpad");
+        }
 
         void Start()
         {
@@ -36,18 +55,70 @@ namespace SmarcGUI.Connections
             ServerAddressInput.interactable = interactable;
             PortInput.interactable = interactable;
             DisconnectButton.interactable = !interactable;
+            PublishControllerToRosToggle.interactable = interactable;
         }
 
         void OnConnect()
         {
             rosCon.Connect(ServerAddress, ServerPort);
             ConnectionInputsInteractable(false);
+            if(PublishControllerToRosToggle.isOn)
+            {
+                rosCon.RegisterPublisher<JoyMsg>(JoyTopic);
+            }
         }
 
         void OnDisconnect()
         {
             rosCon.Disconnect();
             ConnectionInputsInteractable(true);
+        }
+
+        void Update()
+        {
+            if(PublishControllerToRosToggle.isOn)
+            {
+                var dpadVal = dpad.ReadValue<Vector2>();
+                var leftval = lstick.ReadValue<Vector2>();
+                var rightval = rstick.ReadValue<Vector2>();
+                rosCon.Publish(JoyTopic, new JoyMsg
+                {
+                    // https://docs.ros.org/en/iron/p/joy/
+                    axes = new float[] 
+                    {
+                        leftval.x,
+                        leftval.y,
+                        rightval.x,
+                        rightval.y,
+                        lt.ReadValue<float>(),
+                        rt.ReadValue<float>(),    
+                    },
+                    buttons = new int[] 
+                    {
+                        south.ReadValue<int>(),
+                        east.ReadValue<int>(),
+                        west.ReadValue<int>(),
+                        north.ReadValue<int>(),
+                        0, // BACK(SELECT)
+                        0, // GUIDE (Xbox button?)
+                        0, // START
+                        0, // Left stick click
+                        0, // Right stick click
+                        lb.IsPressed()? 1 : 0,
+                        rb.IsPressed()? 1 : 0,
+                        dpadVal.y > 0? 1 : 0, //dpad up
+                        dpadVal.y < 0? 1 : 0, //dpad down
+                        dpadVal.x < 0? 1 : 0, //dpad left
+                        dpadVal.x > 0? 1 : 0, //dpad right
+                        0, // rest is weird stuff like paddles...
+                        0,
+                        0,
+                        0,
+                        0,
+                        0
+                    }
+                });
+            }
         }
 
     }
